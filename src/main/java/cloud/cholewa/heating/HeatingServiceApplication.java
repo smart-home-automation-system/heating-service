8a0eb664d5a89563c3e24c63e6a937c2577f6a2e
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-08-30 23:14:28 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/heating/HeatingServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.heating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeatingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/heating/HeatingServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package cloud.cholewa.heating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class HeatingServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
