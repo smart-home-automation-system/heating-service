ff30dbebb7a08f49bb976c6ae67196f53cecb4c9
Krzysztof Cholewa <magikabdul@users.noreply.github.com>
2024-10-20 21:15:22 +0200

    added global error exception handling (#9)
    * added global error exception handling
    
    * internal dependency update
    
    * fix for tests

---
+++ src/main/java/cloud/cholewa/heating/config/ExceptionHandlerConfig.java
@@ -0,0 +1,39 @@
+package cloud.cholewa.heating.config;
+
+import cloud.cholewa.commons.error.GlobalErrorExceptionHandler;
+import cloud.cholewa.heating.error.RoomNotFoundException;
+import cloud.cholewa.heating.error.processor.RoomNotFoundExceptionProcessor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+import java.util.Map;
+
+@Configuration
+public class ExceptionHandlerConfig {
+
+    @Bean
+    @Order(-2)
+    public GlobalErrorExceptionHandler globalErrorExceptionHandler(
+        final ErrorAttributes errorAttributes,
+        final WebProperties webProperties,
+        final ApplicationContext applicationContext,
+        final ServerCodecConfigurer serverCodecConfigurer
+    ) {
+        GlobalErrorExceptionHandler globalErrorExceptionHandler = new GlobalErrorExceptionHandler(
+            errorAttributes, webProperties.getResources(), applicationContext, serverCodecConfigurer
+        );
+
+        globalErrorExceptionHandler.withCustomErrorProcessor(
+            Map.ofEntries(
+                Map.entry(RoomNotFoundException.class, new RoomNotFoundExceptionProcessor())
+            )
+        );
+
+        return globalErrorExceptionHandler;
+    }
+}
