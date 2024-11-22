52662fbfd79738f95f1ccf5b14d539fa31c4611a
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-11-22 22:48:38 +0100

    home configuration for ground

--- src/main/java/cloud/cholewa/heating/home/config/HomeConfig.java
+++ src/main/java/cloud/cholewa/heating/home/config/HomeConfig.java
@@ -33,9 +33,18 @@ public class HomeConfig {
         final Room bedroom,
         final Room wardrobe,
         final Room bathroomUp,
-        final Room loft
+        final Room loft,
+        final Room livingRoom,
+        final Room cinema,
+        final Room bathroomDown,
+        final Room entrance,
+        final Room garage,
+        final Room sanctum,
+        final Room sauna,
+        final Room garden
     ) {
-        return List.of(office, tobi, livia, bedroom, wardrobe, bathroomUp, loft);
+        return List.of(office, tobi, livia, bedroom, wardrobe, bathroomUp, loft,
+            livingRoom, cinema, bathroomDown, entrance, garage, sanctum, sauna, garden);
     }
 
     @Bean
@@ -146,6 +155,122 @@ public class HomeConfig {
         return Room.builder()
             .name(RoomName.LOFT)
             .temperature(new Temperature())
+            .build();
+    }
+
+    @Bean
+    Room livingRoom() {
+        return Room.builder()
+            .name(RoomName.LIVING_ROOM)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build(),
+                HeaterActor.builder().name(HeaterType.FLOOR).build()
+            ))
+            .openingSensors(List.of(
+                OpeningSensor.builder().name(OpeningType.DOOR).build(),
+                OpeningSensor.builder().name(OpeningType.ENTRANCE_DOOR).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room cinema() {
+        return Room.builder()
+            .name(RoomName.CINEMA)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build()
+            ))
+            .openingSensors(List.of(
+                OpeningSensor.builder().name(OpeningType.WINDOW).build(),
+                OpeningSensor.builder().name(OpeningType.WINDOW2).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room bathroomDown() {
+        return Room.builder()
+            .name(RoomName.BATHROOM_DOWN)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build(),
+                HeaterActor.builder().name(HeaterType.FLOOR).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room entrance() {
+        return Room.builder()
+            .name(RoomName.ENTRANCE)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build()
+            ))
+            .openingSensors(List.of(
+                OpeningSensor.builder().name(OpeningType.ENTRANCE_DOOR).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room garage() {
+        return Room.builder()
+            .name(RoomName.GARAGE)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build()
+            ))
+            .openingSensors(List.of(
+                OpeningSensor.builder().name(OpeningType.GATE_KATE).build(),
+                OpeningSensor.builder().name(OpeningType.GATE_KRIS).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room sanctum() {
+        return Room.builder()
+            .name(RoomName.SANCTUM)
+            .mode(RoomMode.HEATING)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .heaterActors(List.of(
+                HeaterActor.builder().name(HeaterType.RADIATOR).build()
+            ))
+            .openingSensors(List.of(
+                OpeningSensor.builder().name(OpeningType.DOOR).build()
+            ))
+            .build();
+    }
+
+    @Bean
+    Room sauna() {
+        return Room.builder()
+            .name(RoomName.SAUNA)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
+            .build();
+    }
+
+    @Bean
+    Room garden() {
+        return Room.builder()
+            .name(RoomName.GARDEN)
+            .temperature(new Temperature())
+            .humidity(new Humidity())
             .build();
     }
 }
