b4ebbc8fbcf1335c9b13e5a6f5098c813ea5b2b4
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-11-23 12:24:48 +0100

    single room configuration

--- src/main/java/cloud/cholewa/heating/home/api/HomeController.java
+++ src/main/java/cloud/cholewa/heating/home/api/HomeController.java
@@ -4,11 +4,13 @@ import cloud.cholewa.heating.home.api.model.HomeShortResponse;
 import cloud.cholewa.heating.home.service.HomeService;
 import cloud.cholewa.heating.model.BoilerRoom;
 import cloud.cholewa.heating.model.Home;
+import cloud.cholewa.heating.model.Room;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -23,13 +25,13 @@ public class HomeController {
     @GetMapping("/status")
     Mono<ResponseEntity<Home>> getHomeStatus(@RequestBody(required = false) final String fakeRequestBody) {
         log.info("Requesting full home status");
-        return homeService.getHomeStats().map(ResponseEntity::ok);
+        return homeService.getHomeStatus().map(ResponseEntity::ok);
     }
 
     @GetMapping("/status:short")
     Mono<ResponseEntity<HomeShortResponse>> getHomeShortStatus(@RequestBody(required = false) final String fakeRequestBody) {
         log.info("Requesting short home status");
-        return homeService.getHomeShortStats().map(ResponseEntity::ok);
+        return homeService.getHomeShortStatus().map(ResponseEntity::ok);
     }
 
     @GetMapping("/status/boiler")
@@ -38,32 +40,12 @@ public class HomeController {
         return Mono.just(new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED));
     }
 
-//
-//    @GetMapping("configuration/room")
-//    Mono<ResponseEntity<RoomConfigurationResponse>> getRoomConfiguration(
-//        @RequestBody(required = false) final String fakeRequestDueLoggingIssue,
-//        @RequestParam final String roomName
-//    ) {
-//        log.info("Getting room configuration for room {}", roomName);
-//        return homeService.getRoomConfiguration(roomName)
-//            .map(ResponseEntity::ok)
-//            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
-//    }
-//
-//    @GetMapping("configuration/home")
-//    Mono<ResponseEntity<HomeConfigurationResponse>> getHomeConfiguration(
-//        @RequestBody(required = false) final String fakeRequestDueLoggingIssue
-//    ) {
-//        log.info("Getting home configuration");
-//        return homeService.getHomeConfiguration().map(ResponseEntity::ok);
-//    }
-//
-//    @GetMapping
-//    Mono<ResponseEntity<HomeConfigurationResponse>> changeHeatingAllowed(@RequestParam final String heating) {
-//        log.info("Changing heating allowed to: {}", heating);
-//
-//        return heating.equals("on") || heating.equals("off")
-//            ? homeService.changeHomeHeatingState(heating).map(ResponseEntity::ok)
-//            : Mono.error(new RuntimeException("Invalid heating value: " + heating));
-//    }
+    @GetMapping("/status/room/{name}")
+    Mono<ResponseEntity<Room>> getRoomStatus(
+        @PathVariable String name,
+        @RequestBody(required = false) final String fakeRequestBody
+    ) {
+        log.info("Requesting status for room: {}", name);
+        return homeService.getRoomStatusByName(name).map(ResponseEntity::ok);
+    }
 }
--- src/main/java/cloud/cholewa/heating/home/service/HomeService.java
+++ src/main/java/cloud/cholewa/heating/home/service/HomeService.java
@@ -2,11 +2,14 @@ package cloud.cholewa.heating.home.service;
 
 import cloud.cholewa.heating.home.api.mapper.HomeMapper;
 import cloud.cholewa.heating.home.api.model.HomeShortResponse;
+import cloud.cholewa.heating.infrastructure.error.RoomNotFoundException;
 import cloud.cholewa.heating.model.Home;
+import cloud.cholewa.heating.model.Room;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -17,57 +20,19 @@ public class HomeService {
 
     private final Home home;
 
-    public Mono<Home> getHomeStats() {
+    public Mono<Home> getHomeStatus() {
         return Mono.just(home);
     }
 
-    public Mono<HomeShortResponse> getHomeShortStats() {
+    public Mono<HomeShortResponse> getHomeShortStatus() {
         return Mono.justOrEmpty(HomeMapper.toHomeShortResponse(home));
     }
 
-//    public Mono<RoomConfigurationResponse> getRoomConfiguration(final String roomName) {
-//        log.info("Requesting of room configuration for room {}", roomName);
-//
-//        return Mono.just(
-//                home.getRooms().stream()
-//                    .filter(room -> room.getName().name().equalsIgnoreCase(roomName))
-//                    .findAny()
-//            )
-//            .flatMap(room -> room.map(Mono::just)
-//                .orElseThrow(() -> new RoomNotFoundException(roomName)))
-//            .map(RoomMapper::map);
-//    }
-//
-//    public Mono<HomeConfigurationResponse> getHomeConfiguration() {
-//        log.info("Requesting of home configuration");
-//
-//        return Mono.justOrEmpty(
-//            HomeConfigurationResponse.builder()
-//                .isHeatingAllowed(home.isHeatingAllowed())
-//                .boilerRoom(home.getBoiler())
-//                .roomNumber(home.getRooms().size())
-//                .rooms(home.getRooms().stream()
-//                    .map(RoomMapper::map)
-//                    .toList())
-//                .build()
-//        );
-//    }
-//
-//    public Mono<HomeConfigurationResponse> changeHomeHeatingState(final String mode) {
-//        switch (mode) {
-//            case "on" -> home.setHeatingAllowed(true);
-//            case "off" -> home.setHeatingAllowed(false);
-//        }
-//
-//        return Mono.justOrEmpty(
-//            HomeConfigurationResponse.builder()
-//                .isHeatingAllowed(home.isHeatingAllowed())
-//                .boilerRoom(home.getBoiler())
-//                .roomNumber(home.getRooms().size())
-//                .rooms(home.getRooms().stream()
-//                    .map(RoomMapper::map)
-//                    .toList())
-//                .build()
-//        );
-//    }
+    public Mono<Room> getRoomStatusByName(final String name) {
+        return Flux.fromIterable(home.rooms())
+            .filter(room -> room.getName().equalsIgnoreCase(name))
+            .doOnNext(room -> log.info("Founded room with name: {}", room.getName()))
+            .singleOrEmpty()
+            .switchIfEmpty(Mono.error(new RoomNotFoundException("[ " + name.toUpperCase() + " ]" + " not found")));
+    }
 }
