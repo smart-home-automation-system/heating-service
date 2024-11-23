05da4c0d21d22e20959138717b04774900a525d0
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-11-23 16:03:07 +0100

    removing old code

--- src/main/java/cloud/cholewa/heating/amx/service/AmxStatusService.java
+++ src/main/java/cloud/cholewa/heating/amx/service/AmxStatusService.java
@@ -23,44 +23,4 @@ public class AmxStatusService {
             })
             .then();
     }
-
-//
-//
-//    private final Home home;
-//    private final ShellyProService shellyProService;
-//    private final AirHeatingService airHeatingService;
-//
-//    public Mono<ResponseEntity<Void>> updateStatusFromAmx(final DeviceStatusUpdate deviceStatus) {
-//        updateStatus(deviceStatus);
-//
-//        return Mono.just(ResponseEntity.ok().build());
-//    }
-//
-//    private void updateStatus(final DeviceStatusUpdate deviceStatus) {
-//        if (deviceStatus.getRoomName().equals(RoomName.BOILER)) {
-//            final HeatingSource fireplace = home.getBoiler().getHeatingSources().stream()
-//                .filter(heatingSource -> heatingSource.getType().equals(FIREPLACE))
-//                .findFirst().orElseThrow();
-//
-//            fireplace.setUpdateTime(LocalDateTime.now());
-//            fireplace.setTemperature(Double.parseDouble(deviceStatus.getValue()));
-//
-//            shellyProService.controlFirePlacePump(fireplace.getTemperature())
-//                .doOnNext(fireplace::setActive)
-//                .subscribe();
-//        } else {
-//            final Room roomUpdate = home.getRooms().stream()
-//                .filter(room -> room.getName().equals(deviceStatus.getRoomName()))
-//                .findFirst().orElseThrow();
-//
-//            roomUpdate.setTemperatureSensor(TemperatureSensor.builder()
-//                .updateTime(LocalDateTime.now())
-//                .temperature(roomUpdate.getName().equals(RoomName.BATHROOM_DOWN)
-//                    ? Double.parseDouble(deviceStatus.getValue()) + 3 //due to sensor failure temp inc by 3
-//                    : Double.parseDouble(deviceStatus.getValue()))
-//                .build());
-//
-//            airHeatingService.controlHeaterSource(roomUpdate);
-//        }
-//    }
 }
