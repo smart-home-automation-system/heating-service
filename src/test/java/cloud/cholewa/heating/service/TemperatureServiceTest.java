d5f6cafacaeb6ee0d49a2cfa209be0589ec7b0ca
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2026-01-18 10:30:32 +0100

    added unit tests for RabbitTemperatureConsumer and TemperatureService in heating-service

---
+++ src/test/java/cloud/cholewa/heating/service/TemperatureServiceTest.java
@@ -0,0 +1,52 @@
+package cloud.cholewa.heating.service;
+
+import cloud.cholewa.heating.db.model.TemperatureEntity;
+import cloud.cholewa.heating.db.repository.TemperatureRepository;
+import cloud.cholewa.heating.mapper.TemperatureMapper;
+import cloud.cholewa.home.model.TemperatureMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TemperatureServiceTest {
+
+    private static final TemperatureEntity TEMPERATURE_ENTITY =
+        new TemperatureEntity(1L, LocalDateTime.now(), "room", 12.0);
+
+    @Mock
+    private TemperatureMapper temperatureMapper;
+    @Mock
+    private TemperatureRepository temperatureRepository;
+
+    @InjectMocks
+    private TemperatureService sut;
+
+    @Test
+    void should_persist_temperature() {
+        when(temperatureMapper.toEntity(any())).thenReturn(TEMPERATURE_ENTITY);
+
+        when(temperatureRepository.save(any())).thenReturn(Mono.just(TEMPERATURE_ENTITY));
+
+        sut.handleTemperature(new TemperatureMessage())
+            .as(StepVerifier::create)
+            .verifyComplete();
+
+        verify(temperatureMapper, times(1)).toEntity(any());
+        verify(temperatureRepository, times(1)).save(any());
+
+        verifyNoMoreInteractions(temperatureMapper, temperatureRepository);
+    }
+}
